08f1aeb24ca73e235a1a4e2893df0e88bc7fc08a
vsgiridharan <vsgiridharan@gmail.com>
2018-08-01 21:08:54 -0500

    only one case

--- qa-test-mfl/src/test/java/org/naic/mfl/se/challenge/WebTest.java
+++ qa-test-mfl/src/test/java/org/naic/mfl/se/challenge/WebTest.java
@@ -87,27 +87,27 @@ public class WebTest {
 //        assertTrue(driver.getCurrentUrl().contains("controller=my-account"));
 //    }
 
-    @Test
-    public void checkoutTest() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("login"))).click();
-        driver.findElement(By.id("email")).sendKeys(existingUserEmail);
-        driver.findElement(By.id("passwd")).sendKeys(existingUserPassword);
-        driver.findElement(By.id("SubmitLogin")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Women"))).click();
-        driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[1]/div/a[1]/img")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Submit"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Proceed to checkout"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Proceed to checkout"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("processAddress"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("uniform-cgv"))).click();
-        driver.findElement(By.name("processCarrier")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("bankwire"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='cart_navigation']/button"))).click();
-        WebElement heading = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1")));
-        assertEquals("ORDER CONFIRMATION", heading.getText());
-        assertTrue(driver.findElement(By.xpath("//li[@class='step_done step_done_last four']")).isDisplayed());
-        assertTrue(driver.findElement(By.xpath("//li[@id='step_end' and @class='step_current last']")).isDisplayed());
-        assertTrue(driver.findElement(By.xpath("//*[@class='cheque-indent']/strong")).getText().contains("Your order on My Store is complete."));
-        assertTrue(driver.getCurrentUrl().contains("controller=order-confirmation"));
-    }
+//    @Test
+//    public void checkoutTest() {
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("login"))).click();
+//        driver.findElement(By.id("email")).sendKeys(existingUserEmail);
+//        driver.findElement(By.id("passwd")).sendKeys(existingUserPassword);
+//        driver.findElement(By.id("SubmitLogin")).click();
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Women"))).click();
+//        driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[1]/div/a[1]/img")).click();
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Submit"))).click();
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Proceed to checkout"))).click();
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Proceed to checkout"))).click();
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("processAddress"))).click();
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("uniform-cgv"))).click();
+//        driver.findElement(By.name("processCarrier")).click();
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("bankwire"))).click();
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='cart_navigation']/button"))).click();
+//        WebElement heading = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1")));
+//        assertEquals("ORDER CONFIRMATION", heading.getText());
+//        assertTrue(driver.findElement(By.xpath("//li[@class='step_done step_done_last four']")).isDisplayed());
+//        assertTrue(driver.findElement(By.xpath("//li[@id='step_end' and @class='step_current last']")).isDisplayed());
+//        assertTrue(driver.findElement(By.xpath("//*[@class='cheque-indent']/strong")).getText().contains("Your order on My Store is complete."));
+//        assertTrue(driver.getCurrentUrl().contains("controller=order-confirmation"));
+ //   }
 }
